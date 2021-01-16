f4f68f088ab5c232dd70a135d1175b154d1c0a6e
LouaiKB <loukb1998@gmail.com>
2021-01-16 19:32:21 +0100

    adding name controller

--- app/Http/Controllers/RnaSeqController.php
+++ app/Http/Controllers/RnaSeqController.php
@@ -3,7 +3,9 @@
 namespace App\Http\Controllers;
 
 use App\RnaSeq;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+
 
 class RnaSeqController extends Controller 
 {
@@ -17,6 +19,13 @@ class RnaSeqController extends Controller
     public function showOneSeqRegion($id) 
     {
         $query = DB::select("SELECT * FROM seq_region WHERE seq_region_id=$id");
+        return response()->json($query);
+    }
+
+    public function showSeqRegionByName(Request $request)
+    {
+        $name = $request->get("name");
+        $query = DB::select("SELECT * FROM seq_region WHERE name=$name");
         return response()->json($query);
     }
 
